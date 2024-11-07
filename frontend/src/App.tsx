c3cc43938f2e9fd593c012c0a0ecd0511e98ddd2
Piotr Jasinski <piotr.jasinski990@gmail.com>
2024-11-07 15:51:18 +0100

    initial

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <div>
+            <iframe src="https://giphy.com/embed/22kxQ12cxyEww" width="480" height="480"
+                    className="giphy-embed" allowFullScreen></iframe>
+            <p><a href="https://giphy.com/gifs/22kxQ12cxyEww"></a></p></div>
+    </>
+  )
+}
+
+export default App
