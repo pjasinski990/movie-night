9dc99b7720cf9a815e603735f8528b71d0ff4e10
Piotr Jasinski <piotr.jasinski990@gmail.com>
2026-07-06 22:58:50 +0200

    move build to ghcr

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -30,21 +30,5 @@ async function runServer() {
     }
 }
 
-async function runHttps() {
-    const sslOptions = {
-        key: fs.readFileSync(path.resolve(__dirname, './certs/backend.key')),
-        cert: fs.readFileSync(path.resolve(__dirname, './certs/backend.crt')),
-    };
-
-    try {
-        app.use(cors({ origin: `https://${process.env.FRONTEND_DOMAIN}`}));
-        const server = https.createServer(sslOptions, app);
-        server.listen(PORT, () => {
-            console.log(`Https server is running on port ${PORT}`)
-        });
-    } catch (error) {
-        console.error(error);
-    }
-}
-
 run().catch(err => console.error(err));
+
